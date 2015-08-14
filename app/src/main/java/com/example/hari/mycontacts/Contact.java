c79f88b4b19d97ba908c4fc6c6d0915c7199ff81
hariharansubramanian <hariharan1990.s@gmail.com>
2015-08-14 15:03:34 +0530

    Created Contact class with String mName, getters and setters '\n' Declared ListView in layout linked with listView in ContactListActivity

---
+++ app/src/main/java/com/example/hari/mycontacts/Contact.java
@@ -0,0 +1,16 @@
+package com.example.hari.mycontacts;
+
+/**
+ * Created by Nicky on 8/14/2015.
+ */
+public class Contact {
+    private String mName;
+
+    public String getmName() {
+        return mName;
+    }
+
+    public void setmName(String mName) {
+        this.mName = mName;
+    }
+}
