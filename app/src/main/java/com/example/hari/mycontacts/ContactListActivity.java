8314d888ae784775ea1030f99a8c81979892b7c5
hariharansubramanian <hariharan1990.s@gmail.com>
2015-08-14 16:29:50 +0530

    added custom adapter ContactsAdapter extending ArrayAdapter<Contact> to populate the ListView, configured getView() as well

--- app/src/main/java/com/example/hari/mycontacts/ContactListActivity.java
+++ app/src/main/java/com/example/hari/mycontacts/ContactListActivity.java
@@ -4,17 +4,45 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class ContactListActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_contact_list);
         ListView listView= (ListView) findViewById(R.id.contact_list_view);
+        ArrayList<Contact> contacts= new ArrayList<>();
+        listView.setAdapter(new ContactsAdapter(contacts));
+
+        Contact contact1=new Contact();
+        contact1.setmName("Hari");
+
     }
 
+    private class ContactsAdapter extends ArrayAdapter<Contact>{
+        ContactsAdapter(ArrayList<Contact> contacts){
+            super(ContactListActivity.this,R.layout.contact_list_row,R.id.contact_row,contacts);
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            convertView=super.getView(position, convertView, parent);
+            Contact contact=getItem(position);
+            TextView nameTextView= (TextView) convertView.findViewById(R.id.contact_row);
+            nameTextView.setText(contact.getmName());
+
+
+            return convertView;
+        }
+    }
 
 
 
