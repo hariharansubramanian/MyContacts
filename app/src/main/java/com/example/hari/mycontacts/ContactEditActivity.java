2588ae127ba1c82c55c1e3510e353b82deada3f8
hariharansubramanian <hariharan1990.s@gmail.com>
2015-08-20 17:35:02 +0530

    ContactEditActivity: addToSection(int sectionID,ArrayList<String> values) addToSection iterates through all items in ArrayList(email or phonenumber), creates an Edit text for each and sets its LinearLayout.LayoutParams(match,wrap) and sets its Texts ie values.get(i) then finally addView(et) to section given

--- app/src/main/java/com/example/hari/mycontacts/ContactEditActivity.java
+++ app/src/main/java/com/example/hari/mycontacts/ContactEditActivity.java
@@ -4,16 +4,39 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+
+import java.util.ArrayList;
 
 public class ContactEditActivity extends AppCompatActivity {
     public static final String EXTRA = "CEA_EXTRA";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact_edit);
 
-        Contact contact= (Contact) getIntent().getSerializableExtra(EXTRA);
+        Contact contact = (Contact) getIntent().getSerializableExtra(EXTRA);
 
+        EditText editName = (EditText) findViewById(R.id.edit_name);
+        editName.setText(contact.getmName());
+        addToSection(R.id.phonenumber_sectionLinearlayout, contact.getPhoneNumbers());
+        addToSection(R.id.email_sectionLinearlayout, contact.getEmails());
+
+
+    }
+
+    private void addToSection(int sectionID, ArrayList<String> values) {
+        LinearLayout section = (LinearLayout) findViewById(sectionID);
+        for (int i = 0; i < values.size(); i++) {
+            EditText et = new EditText(this);
+            LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+            et.setLayoutParams(lp);
+            et.setText(values.get(i));
+            section.addView(et);
+        }
     }
 
     @Override
